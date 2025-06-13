4774aca1d620f75118c8eadae9306171b04e7f7e
timofey nikitin <tima.nikitin.2005@mail.ru>
2025-06-13 19:14:24 +0300

    Fix imports

--- app/src/main/java/com/example/practice/core_ui/BankCardsList.kt
+++ app/src/main/java/com/example/practice/core_ui/BankCardsList.kt
@@ -1,6 +1,9 @@
 package com.example.practice.coreui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
