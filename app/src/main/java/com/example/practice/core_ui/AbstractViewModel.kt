30563c689942b4c0b970005b6b89cdc4068311ec
timofey nikitin <tima.nikitin.2005@mail.ru>
2025-06-19 23:45:09 +0300

    1) Add state & action interfaces 2) Add App.kt 3) Add @AndroidEntryPoint for MainActivity

--- app/src/main/java/com/example/practice/core_ui/AbstractViewModel.kt
+++ app/src/main/java/com/example/practice/core_ui/AbstractViewModel.kt
@@ -1,13 +1,11 @@
 package com.example.practice.core_ui
 
-import android.service.credentials.Action
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.StateFlow
 
 abstract class AbstractViewModel : ViewModel() {
 
-    abstract val state: StateFlow<Lifecycle.State>
+    abstract val state: StateFlow<State>
 
     open fun doAction(action: Action) = Unit
 }
---
+++ app/src/main/java/com/example/practice/core_ui/Action.kt
@@ -0,0 +1,3 @@
+package com.example.practice.core_ui
+
+interface Action
